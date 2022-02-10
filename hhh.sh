d9292597cde755e29632e611535d4c63e6b45bf8
KarachurinaEkaterina <ekaterina200090@mail.ru>
2022-02-10 18:06:39 +0500

    Message for commit

--- hhh.sh
+++ hhh.sh
@@ -1,9 +1,11 @@
 if (( $1 < $2 ))
 then
 echo "value $1 is too small"
-if (( $2 > $1 ))
+else
+if (( $2 < $1 ))
 then
 echo "value $2 is too small"
-else (( $1 = $2 ))
+else (( $1 == $2 ))
 echo "PERFECT!!!"
+fi
 fi
